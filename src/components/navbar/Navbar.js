bccc016f0d79bd45b4adebf1531641d06579b6ea
Krishna Dhingra <dkrishna1608@gmail.com>
2022-04-29 16:44:10 +0530

    Almost done with the navbar animation

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,59 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import { motion, AnimatePresence } from 'framer-motion'
 import Bell from '../../utilities/navbar-bell.svg'
 import Logo from '../../utilities/navbar-logo.svg'
 
 function Navbar() {
+  const [visible, setVisible] = useState(false)
+
+  const ul = {
+    hidden: { y: '-100%' },
+    visible: {
+      y: '0%',
+      transition: {
+        ease: 'easeOut',
+        duration: 0.2,
+        staggerChildren: 0.07,
+      },
+    },
+    exit: {
+      y: '-100%',
+      transition: {
+        ease: 'easeOut',
+        duration: 0.2,
+      },
+    },
+  }
+
+  const item = {
+    hidden: {
+      opacity: 0,
+      y: -8,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        ease: 'easeOut',
+      },
+    },
+  }
+
+  const navbarLinks = [
+    'About us',
+    'Jobs',
+    'Location',
+    'Book Now',
+    'FAQ',
+    'Blog',
+    'Pricing',
+  ]
   return (
     <nav>
-      <ul>
-        <section className="sm:px-[2rem] lg:px-[4.2rem]">
-          <li>
-            <a href="">
-              <span>About us</span>
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <span>Jobs</span>
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <span>Location</span>
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <span>Book Now</span>
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <span>FAQ</span>
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <span>Blog</span>
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <span>Pricing</span>
-            </a>
-          </li>
-        </section>
-      </ul>
+      <AnimatePresence>
+        {visible && (
+          <motion.ul
+            variants={ul}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          >
+            <section className="sm:px-[2rem] lg:px-[4.2rem]">
+              {navbarLinks.map((link) => {
+                return (
+                  <motion.li variants={item}>
+                    <a href="">
+                      <span>{link}</span>
+                    </a>
+                  </motion.li>
+                )
+              })}
+            </section>
+          </motion.ul>
+        )}
+      </AnimatePresence>
 
       <section className="px-[1rem] sm:px-[2rem] lg:px-[4.2rem]">
-        <div className="bell">
-          <img src={Bell} alt="" />
+        <div className="bell" data-visible={visible}>
+          <svg
+            width="20"
+            height="17"
+            viewBox="0 0 20 17"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M16 7C16 5.4087 15.3679 3.88258 14.2426 2.75736C13.1174 1.63214 11.5913 1 10 1C8.4087 1 6.88258 1.63214 5.75736 2.75736C4.63214 3.88258 4 5.4087 4 7C4 14 1 16 1 16H19C19 16 16 14 16 7Z"
+              stroke="black"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
         </div>
         <div className="logo">
           <img src={Logo} alt="" />
         </div>
-        <div className="hamburger">
+        <div
+          className="hamburger"
+          data-visible={visible}
+          onClick={() => setVisible(!visible)}
+        >
           <div className="line1"></div>
           <div className="line2"></div>
           <div className="line3"></div>
