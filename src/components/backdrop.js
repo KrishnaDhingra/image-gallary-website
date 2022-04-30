f4298420d8e0af0920735b02af1efd03c4c3ae69
Krishna Dhingra <dkrishna1608@gmail.com>
2022-05-01 02:48:36 +0530

    position fixed to backdrop before it was absolute

--- src/components/backdrop.js
+++ src/components/backdrop.js
@@ -5,7 +5,7 @@ const Backdrop = ({ children, onSelect }) => {
   return (
     <motion.div
       onClick={() => onSelect()}
-      className="top-0 left-0 backdrop z-10 absolute w-screen h-screen"
+      className="top-0 left-0 backdrop z-10 fixed w-screen h-screen"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
