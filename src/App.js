d21f4c44d46c7997b214699272bee74380941132
Krishna Dhingra <dkrishna1608@gmail.com>
2022-05-01 02:41:58 +0530

    Made the faq section and also implemented it's animation all is left is to make it responsive and will do it after the designer submits his design

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Enter from './components/enter/Enter'
 import Navbar from './components/navbar/Navbar'
 import WhatHappeningSidePopup from './components/what-happening-sidepopup/what-happening-sidepopup'
 import Home from './pages/home'
+import FAQ from './components/faq/faq'
 function App() {
   return (
     // <Enter />
     <div>
       <Navbar />
-      <Home />
+      <FAQ />
+      {/* <Home /> */}
     </div>
   )
 }
---
+++ src/components/faq/faq.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+import { motion, AnimateSharedLayout, AnimatePresence } from 'framer-motion'
+import './faq.css'
+
+function FAQ() {
+  const data = [
+    {
+      question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      answer:
+        'nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque',
+    },
+    {
+      question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      answer:
+        'nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque',
+    },
+    {
+      question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      answer:
+        'nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque',
+    },
+    {
+      question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      answer:
+        'nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque',
+    },
+    {
+      question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      answer:
+        'nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque',
+    },
+    {
+      question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      answer:
+        'nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque',
+    },
+  ]
+  return (
+    <AnimateSharedLayout>
+      <motion.section layout className="faq-container">
+        <h1>FAQ</h1>
+        <p>Find your answers for the most asked questions</p>
+
+        <div className="faq-inner">
+          <motion.ul layout>
+            {data.map((element) => {
+              return (
+                <Item question={element.question} answer={element.answer} />
+              )
+            })}
+          </motion.ul>
+        </div>
+      </motion.section>
+    </AnimateSharedLayout>
+  )
+}
+
+function Item({ question, answer }) {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <motion.li layout>
+      <motion.div
+        layout="position"
+        className="question-container"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <p>{question}</p>
+        <button>{isOpen ? '-' : '+'}</button>
+      </motion.div>
+      <AnimatePresence>{isOpen && <Content answer={answer} />}</AnimatePresence>
+    </motion.li>
+  )
+}
+function Content({ answer }) {
+  return (
+    <motion.p
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="answer"
+    >
+      {answer}
+    </motion.p>
+  )
+}
+export default FAQ
