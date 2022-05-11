f48befd69de1f5aab52d900bdf9ebda61c2b13e9
Krishna Dhingra <dkrishna1608@gmail.com>
2022-05-12 02:27:48 +0530

    Made the testimonial functionality

---
+++ src/components/testimonials/data.js
@@ -0,0 +1,31 @@
+import SampleImage1 from '../../utilities/sidebar-image.png'
+import SampleImage2 from '../../utilities/sample-image2.png'
+import SampleImage3 from '../../utilities/sample-image3.png'
+import SampleImage4 from '../../utilities/sample-image4.png'
+
+export const Testimonialdata = [
+  {
+    image: SampleImage1,
+    text:
+      'You can also use variant modifiers to target media queries like responsive breakpoints, dark mode, prefers-reduced-motion, and more. For example',
+    fromUser: 'Krishna',
+  },
+  {
+    image: SampleImage2,
+    text:
+      'If you need to use a one-off aspect-ratio value that doesnt make sense to include in your theme, use square brackets to generate a property on the fly using any arbitrary value.',
+    fromUser: 'Ramya',
+  },
+  {
+    image: SampleImage3,
+    text:
+      'By default, Tailwind provides a minimal set of aspect-ratio utilities. You can customize these values by editing theme.aspectRatio or theme.extend.aspectRatio in your',
+    fromUser: 'Jesse',
+  },
+  {
+    image: SampleImage4,
+    text:
+      'You can also use variant modifiers to target media queries like responsive breakpoints, dark mode, prefers-reduced-motion, and more. For example',
+    fromUser: 'Walter',
+  },
+]
--- src/components/testimonials/testimonials.js
+++ src/components/testimonials/testimonials.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { Testimonialdata } from './data.js'
 import { IoIosArrowBack } from 'react-icons/io'
 import { IoIosArrowForward } from 'react-icons/io'
 import SamplePhoto from '../../utilities/sidebar-image.png'
 import './testimonials.css'
 
 function Testimonials() {
+  const [testimonialCounter, setTestimonialCounter] = useState(0)
+
+  const data = Testimonialdata
+
+  const next = () => {
+    if (testimonialCounter !== data.length - 1) {
+      setTestimonialCounter(testimonialCounter + 1)
+      return
+    }
+    setTestimonialCounter(0)
+  }
+  const previous = () => {
+    if (testimonialCounter !== 0) {
+      setTestimonialCounter(testimonialCounter - 1)
+      return
+    }
+    setTestimonialCounter(data.length - 1)
+  }
   return (
     <main className="testimonials-container mx-auto">
       <article className="px-4 md:px-0 col-start-1 col-end-2">
@@ -18,31 +38,53 @@ function Testimonials() {
         </p>
       </article>
 
-      <div className="testimonial flex flex-col sm:flex-row gap-8">
-        <IoIosArrowBack className="hidden sm:inline shrink-0 text-xl hover:text-gray-400" />
-        <img
-          className="self-start m-auto max-w-[19rem] sm:max-w-[13rem] lg:max-w-[17rem] object-cover aspect-video sm:aspect-square"
-          src={SamplePhoto}
-          alt=""
+      <div className="w-full overflow-hidden testimonial flex flex-col sm:flex-row gap-8">
+        <Testimonial
+          next={next}
+          previous={previous}
+          image={data[testimonialCounter].image}
+          text={data[testimonialCounter].text}
+          fromUser={data[testimonialCounter].fromUser}
         />
-        {/* this div will take affect only in small screens because only in the small screens will beg the arrows visible */}
-        <div className="flex gap-6">
-          <IoIosArrowBack className="inline sm:hidden shrink-0 text-xl hover:text-gray-400" />
-          <div className="flex flex-col gap-2 justify-center">
-            {/* these arrows will appear in the mobile view */}
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing lit. Maxime
-              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-              voluptatum laborum numquam blanditiis
-            </p>
-            <h3 className="font-semibold">Ramya</h3>
-          </div>
-          <IoIosArrowForward className="inline sm:hidden shrink-0 text-xl hover:text-gray-400" />
-        </div>
-        {/* these arrows will disappear in mobile view */}
-        <IoIosArrowForward className="hidden sm:inline shrink-0 text-xl hover:text-gray-400" />
       </div>
     </main>
+  )
+}
+
+function Testimonial({ image, text, fromUser, next, previous }) {
+  return (
+    <>
+      <IoIosArrowBack
+        onClick={previous}
+        className="hidden sm:inline shrink-0 text-xl hover:text-gray-400"
+      />
+      <img
+        className="self-start m-auto max-w-[19rem] sm:max-w-[13rem] lg:max-w-[17rem] object-cover aspect-video sm:aspect-square"
+        src={image}
+        alt=""
+      />
+      {/* this div will take affect only in small screens because only in the small screens will beg the arrows visible */}
+      <div className="flex gap-6">
+        <IoIosArrowBack
+          onClick={previous}
+          className="inline sm:hidden shrink-0 text-xl hover:text-gray-400"
+        />
+        <div className="flex flex-col gap-2 justify-center">
+          {/* these arrows will appear in the mobile view */}
+          <p>{text}</p>
+          <h3 className="font-semibold">{fromUser}</h3>
+        </div>
+        <IoIosArrowForward
+          onClick={next}
+          className="inline sm:hidden shrink-0 text-xl hover:text-gray-400"
+        />
+      </div>
+      {/* these arrows will disappear in mobile view */}
+      <IoIosArrowForward
+        onClick={next}
+        className="hidden sm:inline shrink-0 text-xl hover:text-gray-400"
+      />
+    </>
   )
 }
 export default Testimonials
