922f1cdb93146b4829377dee3a4fadb4f26f498d
Krishna Dhingra <dkrishna1608@gmail.com>
2022-05-19 14:33:03 +0530

    Made the book now slider

---
+++ src/components/book-now-slider/book-now-slider.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import './book-now-slider.css'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper'
+import 'swiper/css/pagination'
+
+function BookNowSlider() {
+  const items = [
+    'https://swiperjs.com/demos/images/nature-1.jpg',
+    'https://swiperjs.com/demos/images/nature-2.jpg',
+    'https://swiperjs.com/demos/images/nature-3.jpg',
+    'https://swiperjs.com/demos/images/nature-4.jpg',
+    'https://swiperjs.com/demos/images/nature-5.jpg',
+    'https://swiperjs.com/demos/images/nature-7.jpg',
+    'https://swiperjs.com/demos/images/nature-1.jpg',
+    'https://swiperjs.com/demos/images/nature-2.jpg',
+    'https://swiperjs.com/demos/images/nature-3.jpg',
+    'https://swiperjs.com/demos/images/nature-4.jpg',
+    'https://swiperjs.com/demos/images/nature-5.jpg',
+    'https://swiperjs.com/demos/images/nature-7.jpg',
+    'https://swiperjs.com/demos/images/nature-1.jpg',
+    'https://swiperjs.com/demos/images/nature-2.jpg',
+    'https://swiperjs.com/demos/images/nature-3.jpg',
+    'https://swiperjs.com/demos/images/nature-4.jpg',
+    'https://swiperjs.com/demos/images/nature-5.jpg',
+    'https://swiperjs.com/demos/images/nature-7.jpg',
+  ]
+
+  return (
+    <Swiper
+      grabCursor={true}
+      centeredSlides={true}
+      centeredSlidesBounds={true}
+      slidesPerView={'auto'}
+      spaceBetween={25}
+      pagination={true}
+      modules={[Pagination]}
+      className="book-now-swiper"
+    >
+      {items.map((item) => {
+        return (
+          <SwiperSlide className="book-now-swiper-slide">
+            <img src={item} />
+          </SwiperSlide>
+        )
+      })}
+    </Swiper>
+  )
+}
+export default BookNowSlider
--- src/pages/book-now.js
+++ src/pages/book-now.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import BookNowTopSection from '../components/book-now-top-section/book-now-top-section'
 import BookNowContactForm from '../components/book-now-contact-form/book-now-contact-form'
+import BookNowSlider from '../components/book-now-slider/book-now-slider'
 function BookNow() {
   return (
     <main>
       <BookNowTopSection />
       <BookNowContactForm />
+      <BookNowSlider />
     </main>
   )
 }
